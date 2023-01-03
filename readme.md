ef43b3aa38e643bbf2764f9976c0149d5901951d
Martin Burolla <mburolla@xpanxion.com>
2023-01-03 12:12:08 -0500

    update

--- readme.md
+++ readme.md
@@ -4,7 +4,8 @@ Assignment repo for Java I.
 # Getting Started
 - Clone this repo into a local directory on your computer
 - Open IntelliJ
-- Open the `pom.xml` file in IntelliJ
+- Navigate to the `pom.xml` file in IntelliJ and select "Open as Project"
 - Create a Main class based from the `Main.java.template` file here: `./src/main/java/com/xpanxion/assignments/`
   - Replace the student id with your student id
-- 
+- Create a configuration named "Develop" for this class
+- Run the app (Press the green play button)
--- src/main/java/com/xpanxion/assignments/stu1/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/stu1/JavaOne.java
@@ -24,31 +24,31 @@ public class JavaOne {
         System.out.println("Student 1: ex3.");
     }
 
-    public void ex5() {
+    public void ex4() {
         System.out.println("Student 1: ex4.");
     }
 
-    public void ex6() {
+    public void ex5() {
         System.out.println("Student 1: ex5.");
     }
 
-    public void ex7() {
+    public void ex6() {
         System.out.println("Student 1: ex6.");
     }
 
-    public void ex8() {
+    public void ex7() {
         System.out.println("Student 1: ex7.");
     }
 
-    public void ex9() {
+    public void ex8() {
         System.out.println("Student 1: ex8.");
     }
 
-    public void ex10() {
+    public void ex9() {
         System.out.println("Student 1: ex9.");
     }
 
-    public void ex4() {
+    public void ex10() {
         System.out.println("Student 1: ex10.");
     }
 
